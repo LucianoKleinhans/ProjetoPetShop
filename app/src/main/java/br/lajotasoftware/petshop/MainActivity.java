cc6955261489882a797b95198bd960a5e0774a66
Júnior <juniorott17@gmail.com>
2021-10-24 16:33:48 -0400

    PetShop Commit#2
    Segundo Commit PetShop - Adição das telas restantes (cadastro dono e pet), correção de hardcoded text, logo tela inicial fornecido por @LucianoKleinhans

--- app/src/main/java/br/lajotasoftware/petshop/MainActivity.java
+++ app/src/main/java/br/lajotasoftware/petshop/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.TelaMain);
+        setContentView(R.layout.telamain);
         firebaseDatabase= FirebaseDatabase.getInstance();
         databaseReference= firebaseDatabase.getReference();
     }
